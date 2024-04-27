3da1dd976770d46a9af31220c25b6e66f9c2e7fd
tanha27 <tanhasultana27usa@gmail.com>
2024-04-26 18:48:29 -0700

    finished dom count project

---
+++ index.js
@@ -0,0 +1,60 @@
+/*function handleAdd(){
+    var element = document.getElementById('balance');
+    var currentValue = parseInt(element.innerText);
+    var sum = currentValue + 1;
+    element.innerText = sum;
+}
+
+
+function handleDelete(){
+    var element = document.getElementById('balance');
+    var currentValue = parseInt(element.innerText);
+    var sub = currentValue - 1;
+    element.innerText = sub;
+}*/
+
+
+
+
+function handleAdd(){
+var element = document.getElementById('balance');
+var currentValue = parseInt(element.innerText);
+ var sum = currentValue + 1;
+ element.innerText = sum;
+}
+
+
+
+
+function handleDelete(){
+    var element = document.getElementById('balance');
+    var currentValue = parseInt(element.innerText);
+    var sub = currentValue - 1;
+    element.innerText = sub;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
